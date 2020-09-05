eba60dae8ebf46149371c9454c2992c043100432
J4102 <JYang8501@gmail.com>
2020-09-05 14:41:35 -0400

    Create basic Elderly, young, and main activity

--- app/src/main/java/com/example/elderlycaregamified/MainActivity.java
+++ app/src/main/java/com/example/elderlycaregamified/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.elderlycaregamified;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void startYoungActivity(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, YoungActivity.class);
+        startActivity(intent);
+    }
+
+    public void startOldActivity(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, ElderlyActivity.class);
+        startActivity(intent);
     }
 }
